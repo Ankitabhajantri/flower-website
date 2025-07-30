0be229126e60878f034bdc76b1edb1f244c02034
Ankita V Bhajantri <ankita@example.com>
2025-07-30 13:16:26 +0530

    Initial website commit

---
+++ script8.js
@@ -0,0 +1,9 @@
+function showMessage(){
+    document.getElementById('send message').textContent="Thankyou for contacting us!";
+}
+
+function buyProduct() {
+  alert("Redirecting to payment...");
+  window.location.href = "checkout.html"; // Create this page separately
+}
+
